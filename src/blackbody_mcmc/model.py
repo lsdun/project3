c0f8d4b161bb04e64ecc937fd961858a342cc3c9
juliajvbaumgarten <juliaxbaumgarten@gmail.com>
2025-11-16 17:19:35 -0500

    Create model.py

---
+++ src/blackbody_mcmc/model.py
@@ -0,0 +1,8 @@
+"""Physical and statistical model for blackbody radiation"""
+
+from __future__ import annotations
+
+from typing import Sequence
+
+import numpy as np
+from scipy import constants as const
