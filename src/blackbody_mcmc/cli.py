d6c912d67cfdb2fc6d70eff664bcda22147bcdb9
lsdun <lasdun@syr.edu>
2025-11-19 20:21:35 -0500

    Update cli.py

--- src/blackbody_mcmc/cli.py
+++ src/blackbody_mcmc/cli.py
@@ -9,8 +9,8 @@ import numpy as np
 
 from .data import load_data
 from .diagnostics import autocorrelation, gelman_rubin
-from .mcmc_library import run_emcee, EmceeResult
-from .mcmc_manual import run_mcmc, MCMResult
+from .mcmc_library import run_emcee
+from .mcmc_manual import run_mcmc
 from .model import log_posterior, model_intensity
 from .plotting import plot_data_and_model, plot_histograms, plot_traces
 
